4c441a638ae043a0961fe0201b183be6be8b042e
sunzhenyi <sunzhenyi@buaa.edu.cn>
2019-06-17 08:27:20 +0800

    可刷新listview

---
+++ app/src/main/java/sun/com/android_ui_demos/pull_refresh_listview/PullRefreshListView.java
@@ -0,0 +1,17 @@
+package sun.com.android_ui_demos.pull_refresh_listview;
+
+import android.view.MotionEvent;
+import android.widget.ListView;
+
+/**
+ * Created by sunzy on 2019/6/16.
+ */
+
+public class PullRefreshListView extends ListView {
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(ev);
+    }
+}
